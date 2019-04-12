d4af4d1d0808eea0fe39345ee6eb97ca76fdcadf
Nat-Reid <nathanielreid@gmail.com>
2019-04-11 17:21:21 -0700

    rotating

--- src/components/Shape.js
+++ src/components/Shape.js
@@ -5,7 +5,7 @@ class Shape extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			coordArry: [ [ 1, 5 ], [ 1, 6 ], [ 2, 5 ], [ 2, 6 ] ],
+			coordArry: [ [ 1, 4 ], [ 1, 5 ], [ 1, 6 ], [ 1, 7 ] ],
 			centerPoint: [ 1.5, 5.5 ]
 		};
 	}
@@ -52,7 +52,7 @@ class Shape extends Component {
 
 	moveDown = () => {
 		this.state.centerPoint[0]++;
-		console.log('in the move right function');
+		console.log('in the move down function');
 		let down = this.state.coordArry.map((coord) => {
 			return [ coord[0] + 1, coord[1] ];
 		});
@@ -61,13 +61,20 @@ class Shape extends Component {
 		});
 	};
 
-	rotate = (cx,cy,x,y) => {
-    let radians = (Math.PI / 180) * 90,
-        cos = Math.cos(radians),
-        sin = Math.sin(radians),
-        nx = (cos * (x - cx)) + (sin * (y - cy)) + cx,
-        ny = (cos * (y - cy)) - (sin * (x - cx)) + cy;
-    return [nx, ny];
+	rotate = () => {
+		console.log("ROTATING ")
+		this.setState(prevState =>{
+			return {coordArry: prevState.coordArry.map(coord => actualrotatefunc(this.state.centerPoint,coord))}
+		})
+
+		function actualrotatefunc([cx,cy],[x,y]){
+	    let radians = (Math.PI / 180) * 90,
+	        cos = Math.cos(radians),
+	        sin = Math.sin(radians),
+	        nx = (cos * (x - cx)) + (sin * (y - cy)) + cx,
+	        ny = (cos * (y - cy)) - (sin * (x - cx)) + cy;
+	    return [nx, ny];
+		}
 	}
 
 	render() {
