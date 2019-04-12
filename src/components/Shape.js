264165bbab6b11b4dfc10ea80c6f2e183cac596c
Tiffany <Tafby88@gmail.com>
2019-04-12 09:58:32 -0700

    stopped the shape from moving past the walls of the grid

--- src/components/Shape.js
+++ src/components/Shape.js
@@ -11,41 +11,45 @@ class Shape extends Component {
 	}
 	componentDidMount = () => {
 		document.addEventListener('keydown', (event) => {
-			switch(event.keyCode){
-				case 37 :
+			switch (event.keyCode) {
+				case 37:
 					this.moveLeft();
 					break;
-				case 39 :
+				case 39:
 					this.moveRight();
 					break;
-				case 40 :
+				case 40:
 					this.moveDown();
 					break;
-				case 32 :
+				case 32:
 					this.rotate();
 					break;
 			}
 		});
-		let intervalID = window.setInterval(this.moveDown, 500)
-	}
+		let intervalID = window.setInterval(this.moveDown, 500);
+	};
 
 	moveLeft = () => {
-		console.log('in the move left function');
 		this.state.centerPoint[1]--;
 		let left = this.state.coordArry.map((coord) => {
 			return [ coord[0], coord[1] - 1 ];
 		});
+		if (!this.collision(left)) {
+			return;
+		}
 		this.setState({
 			coordArry: left
 		});
 	};
 
 	moveRight = () => {
-		console.log('in the move right function');
 		this.state.centerPoint[1]++;
 		let right = this.state.coordArry.map((coord) => {
 			return [ coord[0], coord[1] + 1 ];
 		});
+		if (!this.collision(right)) {
+			return;
+		}
 		this.setState({
 			coordArry: right
 		});
@@ -53,30 +57,40 @@ class Shape extends Component {
 
 	moveDown = () => {
 		this.state.centerPoint[0]++;
-		console.log('in the move down function');
 		let down = this.state.coordArry.map((coord) => {
 			return [ coord[0] + 1, coord[1] ];
 		});
+		if (!this.collision(down)) {
+			return;
+		}
 		this.setState({
 			coordArry: down
 		});
 	};
 
 	rotate = () => {
-		console.log("ROTATING ")
-		this.setState(prevState =>{
-			return {coordArry: prevState.coordArry.map(coord => actualrotatefunc(this.state.centerPoint,coord))}
-		})
+		this.setState((prevState) => {
+			return { coordArry: prevState.coordArry.map((coord) => actualrotatefunc(this.state.centerPoint, coord)) };
+		});
 
-		function actualrotatefunc([cx,cy],[x,y]){
-	    let radians = (Math.PI / 180) * 90,
-	        cos = Math.cos(radians),
-	        sin = Math.sin(radians),
-	        nx = (cos * (x - cx)) + (sin * (y - cy)) + cx,
-	        ny = (cos * (y - cy)) - (sin * (x - cx)) + cy;
-	    return [nx, ny];
+		function actualrotatefunc([ cx, cy ], [ x, y ]) {
+			let radians = Math.PI / 180 * 90,
+				cos = Math.cos(radians),
+				sin = Math.sin(radians),
+				nx = cos * (x - cx) + sin * (y - cy) + cx,
+				ny = cos * (y - cy) - sin * (x - cx) + cy;
+			return [ nx, ny ];
 		}
-	}
+	};
+
+	collision = (collisionCoords) => {
+		// debugger;
+		return collisionCoords.every((coord) => actualcollisionfunc(coord));
+
+		function actualcollisionfunc(coord) {
+			return coord[1] >= 1 && coord[1] <= 10 && coord[0] <= 20;
+		}
+	};
 
 	render() {
 		return (
