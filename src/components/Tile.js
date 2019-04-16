0d969951e329ff39b29959fb9c0162a8d684ddcd
Tiffany <Tafby88@gmail.com>
2019-04-16 14:40:00 -0700

    updated colors

--- src/components/Tile.js
+++ src/components/Tile.js
@@ -1,15 +1,10 @@
 import React, { Component } from 'react';
 
 class Tile extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			color: "#E0FBFC"
-		}
-	}
+
 	
 	render() {
-		return <span className="tile"  style={{ gridArea: `${this.props.row} / ${this.props.column} / span 1 / span 1`, backgroundColor: this.state.color }} />;
+		return <span className="tile"  style={{ gridArea: `${this.props.row} / ${this.props.column} / span 1 / span 1` }} />;
 	}
 }
 
