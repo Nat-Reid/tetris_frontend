f7ab154fa52d9fd8326451608aea56734e715893
Nat-Reid <nathanielreid@gmail.com>
2019-04-15 17:45:05 -0700

    reenabled falling

--- src/components/Shape.js
+++ src/components/Shape.js
@@ -12,7 +12,7 @@ class Shape extends Component {
 	componentDidMount = () => {
 		this.setShape();
 		document.addEventListener('keydown', this.keyListener);
-		// this.intervalID = window.setInterval(this.moveDown, 500); *************put back very important this is commented because we are debugging
+		this.intervalID = window.setInterval(this.moveDown, 500);
 		console.log("current shapes in didmount", this.props.currentShapes)
 	};
 
